e610c6c0d96a87edb551abbdc7d797f7b38685f4
Christian Sandoya <crisandoya@outlook.com>
2026-09-05 06:55:29 -0500

    Week 01 Completed

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const fullDate = new Date();
+const currentYear = fullDate.getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = document.lastModified;
